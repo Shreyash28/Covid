f77734c359afdd756d31e3bb287570c52f6e6f32
Shreyash <67950550+Shreyash28@users.noreply.github.com>
2021-07-27 12:18:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const https = require('https');
 const bodyParser = require("body-parser");
@@ -9,34 +10,33 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-
-    // const url = "https://api.rootnet.in/covid19-in/hospitals/beds";
-    // https.get(url, function(response) {
-    //     console.log(res.statusCode);
-    //     response.on("data", function(data) {
-    //         console.log(JSON.parse(data));
-
-    //     })
-    // })
     var axios = require("axios").default;
 
     var options = {
+
         method: 'GET',
         url: 'https://corona-virus-world-and-india-data.p.rapidapi.com/api_india',
         headers: {
-            'x-rapidapi-key': '0996084accmsh8af2dae1d07d60ep1bb034jsnaabce7f43771',
-            'x-rapidapi-host': 'corona-virus-world-and-india-data.p.rapidapi.com'
+            'x-rapidapi-key': process.env.RAPID_API_KEY,
+            'x-rapidapi-host': process.env.RAPID_API_HOST
         }
     };
 
     axios.request(options).then(function(response) {
-
         var CCase = response.data.total_values.confirmed;
         var ACase = response.data.total_values.active
         var DCase = response.data.total_values.deaths
         var RCase = response.data.total_values.recovered
+        var stateList = response.data.state_wise;
+        res.send(stateList);
 
-        res.render("livecases", { Confirmed: CCase, Active: ACase, Deceased: DCase, Recovered: RCase });
+        res.render("livecases", {
+            Confirmed: CCase,
+            Active: ACase,
+            Deceased: DCase,
+            Recovered: RCase,
+            // StateWise: state
+        });
     }).catch(function(error) {
         console.error(error);
     });
